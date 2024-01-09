4695d0d261714590bfe00c7d98208a2d70ebb0d4
Marco Rocheles <marcorocheles@hotmail.com>
2024-01-09 01:37:51 -0300

    Adicionando tratativa para Exception Handler

---
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/exceptions/ExceptionResponse.java
@@ -0,0 +1,34 @@
+package br.com.erudio.exceptions;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class ExceptionResponse implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Date timestamp;
+	private String message;
+	private String detail;
+	
+	public ExceptionResponse(Date timestamp, String message, String detail) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.detail = detail;
+	}
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public String getDetail() {
+		return detail;
+	}
+	
+	
+}
